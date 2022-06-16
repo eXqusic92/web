d55a9c05d4994a82009a0345b0f4d9677c4327b8
eXqusic92 <pavlo.greva@gmail.com>
2022-06-16 12:08:16 +0300

    commit

--- lab5/app/src/test/Error.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import Error from '../Error';
-
-test('tests error', () => {
-  render(<Error message="randomMessage"/>);
-  expect(screen.getByText("randomMessage")).toBeInTheDocument();
-});
--- lab5/app/src/test/Success.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen, fireEvent, getByText } from '@testing-library/react';
-import Success from '../Success';
-
-test('tests success', () => {
-  render(<Success message="randomMessage"/>);
-  expect(screen.getByText("randomMessage")).toBeInTheDocument();
-});
